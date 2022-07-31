1cbd0d792507d440c034d176f71845ef22fac2af
Caste <www.jrctabora@gmail.com>
2022-07-30 18:46:20 -0600

    New Login Design

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,192 +1,168 @@
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, ImageBackground, Image } from 'react-native';
 import React, { useState, useContext, useEffect } from 'react';
 import UsuarioContext from '../context/UsuarioContext';
 import Mensaje from '../components/Mensaje';
 import Cargando from '../components/Cargando';
 
+import { paletaDeColores } from '../styles/colores'
+import { AntDesign } from '@expo/vector-icons';
 
-const Login = ({navigation}) => {
-	const [usuarioText, setUsuarioText] = useState("");
-	const [contrasena, setContrasena] = useState("");
-	const titulo= 'Inicio de sesión'
-	const { setLogin, msj, sesionIniciada, tokenValidado }=useContext(UsuarioContext);
-	const [cargarDatos, setCargarDatos] = React.useState(false);
-	useEffect(() => {
-		setCargarDatos(true);
-	}, []);
-	/*useEffect(() =>{
-	  //Mensaje({titulo: titulo, msj: msj});
-	  console.log("hola");
-	  console.log(sesionIniciada);
-	  if(tokenValidado && sesionIniciada){
-		//Mensaje({titulo: titulo, msj: msj});
-		navigation.navigate('Menu');
-	  }
-	}, [iniciarSesion]);*/
-	const iniciarSesion = async () => {
-		if (usuarioText.length < 3){
-			Mensaje({titulo: titulo, msj: 'Escriba el nombre de usuario'});
-		}else if(contrasena.length < 6){
-			Mensaje({titulo: titulo, msj: 'Escriba la contrasena'});
-		}
-		else{
-			setCargarDatos(false);
-			await setLogin({usuario: usuarioText, contrasena: contrasena});
-			setCargarDatos(true);
-		}
-	};
-	if(cargarDatos){
-		return (
-			<View style={styles.contenedor}>
-				<View style={styles.contenedorLogin}>
-					<View style={styles.contenedorTitulo}>
-						<Text style={styles.tituloLogin}>Menú Digital</Text>
-					</View>
-					<View style={[styles.contenedorControles, styles.sombraControles]}>
-						<View style={styles.controles}>
-							<TextInput
-								placeholder="Escriba el nombre de usuario"
-								style={styles.entradas}
-								value={usuarioText}
-								onChangeText={setUsuarioText}
-								autoFocus={true}
-							>
-							</TextInput>
-							<TextInput
-								placeholder="Escriba la contraseña"
-								style={styles.entradas}
-								passwordRules=""
-								secureTextEntry={true}
-								value={contrasena}
-								onChangeText={setContrasena}
-							>
-							</TextInput>
-						</View>
-						<View style={styles.contenedorBotones}>
-							<View style={styles.boton}>
-								<Button title="Iniciar Sesión"
-										onPress={iniciarSesion}
-										color={'orange'}
-								></Button>
-							</View>
-							<View style={styles.boton}>
-								<Button title="Cerrar"
-								></Button>
-							</View>
-						</View>
-						<View style={styles.contenedorBotonesRedes}>
-							<View style={styles.botonRedes}>
-								<Button
-									title="Inicio"
-									onPress={iniciarSesion}
-								></Button>
-							</View>
-							<View style={styles.botonRedes}>
-								<Button
-									title="Google" color={"#dc3545"}
-								>
-								</Button>
-							</View>
-						</View>
+const Login = ({ navigation }) => {
+    const [usuarioText, setUsuarioText] = useState("");
+    const [contrasena, setContrasena] = useState("");
+    const titulo = 'Inicio de sesión'
+    const { setLogin, msj, sesionIniciada, tokenValidado } = useContext(UsuarioContext);
+    const [cargarDatos, setCargarDatos] = React.useState(false);
+    useEffect(() => {
+        setCargarDatos(true);
+    }, []);
+    /*useEffect(() =>{
+      //Mensaje({titulo: titulo, msj: msj});
+      console.log("hola");
+      console.log(sesionIniciada);
+      if(tokenValidado && sesionIniciada){
+        //Mensaje({titulo: titulo, msj: msj});
+        navigation.navigate('Menu');
+      }
+    }, [iniciarSesion]);*/
+    const iniciarSesion = async () => {
+        if (usuarioText.length < 3) {
+            Mensaje({ titulo: titulo, msj: 'Escriba el nombre de usuario' });
+        } else if (contrasena.length < 6) {
+            Mensaje({ titulo: titulo, msj: 'La contraseña debe ser mayor a 6 caracteres' });
+        }
+        else {
+            setCargarDatos(false);
+            await setLogin({ usuario: usuarioText, contrasena: contrasena });
+            setCargarDatos(true);
+        }
+    };
+    if (cargarDatos) {
+        return (
+            <View style={styles.container}>
 
-					</View>
-				</View>
-			</View>
-		);
-	}
-	else{
-		return <Cargando texto="Iniciando Sesión"/>;
-	}
+                <ImageBackground source={require('../assets/Login/background.png')} resizeMode="repeat" imageStyle={{ opacity: 0.1 }} style={styles.backgroundImage}>
+                    <View style={styles.loginForm}>
+                        <View style={styles.loginFormHeader}>
+                            <Image source={require('../assets/logoSigres.png')} style={styles.Logo}></Image>
+                            <Text style={styles.logoText}>Sistema de Gestión para Restaurantes</Text>
+                        </View>
+
+                        <View style={{ marginTop: 20 }}>
+                            <Text style={styles.label}>Usuario</Text>
+                            <View style={{ flexDirection: 'row' }}>
+                                <AntDesign name="user" style={{
+                                    fontSize: 24,
+                                    color: paletaDeColores.blue,
+                                    padding: 10,
+                                    backgroundColor: paletaDeColores.blue + 10,
+                                    borderTopLeftRadius: 10,
+                                    borderBottomLeftRadius: 10,
+                                }} />
+                                <TextInput
+                                    value={usuarioText}
+                                    onChangeText={setUsuarioText}
+                                    autoFocus={true}
+                                    style={styles.input}
+                                    placeholder='e.j. Admin' selectionColor="#777777" />
+                            </View>
+                        </View>
+                        <View style={{ marginTop: 20 }}>
+                            <Text style={styles.label}>Contraseña</Text>
+                            <View style={{ flexDirection: 'row' }}>
+                                <AntDesign name="lock" style={{
+                                    fontSize: 24,
+                                    color: paletaDeColores.blue,
+                                    padding: 10,
+                                    backgroundColor: paletaDeColores.blue + 10,
+                                    borderTopLeftRadius: 10,
+                                    borderBottomLeftRadius: 10,
+                                }} />
+                                <TextInput
+                                    secureTextEntry
+                                    style={styles.input}
+                                    placeholder='•••••••••'
+                                    selectionColor="#777777"
+                                    passwordRules=""
+                                    value={contrasena}
+                                    onChangeText={setContrasena} />
+                            </View>
+                        </View>
+                        <View style={{ marginTop: 20 }}>
+                            <TouchableOpacity style={styles.botonIniciarSesion} onPress={iniciarSesion}>
+                                <Text style={styles.botonIniciarSesionTexto}>Iniciar Sesión</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </ImageBackground>
+            </View>
+        );
+    }
+    else {
+        return <Cargando texto="Iniciando Sesión" />;
+    }
 
 };
 const styles = StyleSheet.create({
-	contenedor: {
-		backgroundColor: 'gray',
-		alignItems: 'center',
-		justifyContent: "center",
-		margin:0,
-		padding: 20,
-		width:"100%",
-		height:"100%",
-	},
-	contenedorLogin: {
-		alignItems: "stretch",
-		justifyContent: 'center',
-		height: 530,
-		width: 360,
-	},
-	contenedorTitulo: {
-		flex: 1,
-		flexDirection:"column",
-		alignItems: "center",
-		justifyContent: "flex-end",
-	},
-	contenedorControles: {
-		flex: 3,
-		flexDirection:"column",
-		alignItems: "stretch",
-		justifyContent:"center",
-		borderWidth: 1,
-		borderColor: "#dedede",
-		borderRadius: 1,
-		backgroundColor:'white',
-		padding:10,
-	},
-	sombraControles: {
-		shadowColor: 'gray',
-		shadowOffset: {width: -2, height: 4},
-		shadowOpacity: 0.2,
-		shadowRadius: 5,
-	},
-	tituloLogin: {
-		color: 'black' ,
-		fontSize: 20,
-		fontWeight: "500",
-	},
-	controles:{
-		flex:4,
-		//backgroundColor: "#29291f",
-		marginBottom: 2,
-		paddingTop:2,
-		paddingLeft:2,
-		paddingRight:2,
-	},
-	contenedorBotones:{
-		flex:1,
-		padding: 10,
-		justifyContent:"space-evenly",
-		flexDirection: "row",
-	},
-	contenedorBotonesRedes:{
-		flex:2,
-		padding: 10,
-		justifyContent:"space-evenly",
-		flexDirection: "column",
-	},
-	boton:{
-		flex:1,
-		alignItems:"stretch",
-		marginLeft:10,
-		marginRight:10,
-	},
-	botonRedes:{
-		flex:1,
-		alignItems:"stretch",
-		margin:5,
-	},
-	entradas:{
-		flex:1,
-		alignItems:"stretch",
-		margin:10,
-		padding:10,
-		fontSize: 20,
-		fontWeight:"400",
-		color: "#495057",
-		backgroundColor:"#fff",
-		borderWidth:1,
-		borderStyle:"solid",
-		borderColor: "#ced4da",
-		borderRadius: 15,
-	}
+    container: {
+        flex: 1,
+    },
+
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover', // or 'stretch',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    loginForm: {
+        width: '80%',
+        backgroundColor: paletaDeColores.white,
+        padding: 10,
+        borderRadius: 20,
+    },
+    loginFormHeader: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        borderBottomWidth: 1,
+        borderBottomColor: paletaDeColores.backgroundMedium,
+    },
+    logoText: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        maxWidth: '70%',
+    },
+
+    Logo: {
+        height: 70,
+        width: 70,
+    },
+    label: {
+        fontSize: 14,
+        textAlign: 'left',
+        marginBottom: 8,
+        marginHorizontal: 10,
+
+    },
+    input: {
+        flex: 1,
+        backgroundColor: paletaDeColores.backgroundLight,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        padding: 10,
+    },
+    botonIniciarSesion: {
+        padding: 6,
+        borderRadius: 20,
+        borderColor: 'white',
+        backgroundColor: paletaDeColores.blue,
+        borderWidth: 1,
+        alignItems: 'center'
+    },
+    botonIniciarSesionTexto: {
+        color: paletaDeColores.white,
+        fontWeight: '600',
+        fontSize: 16
+    }
 });
 export default Login;
