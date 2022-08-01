22ab7f6bb0e014ba6402248a171627ba015a14b5
Elvis Alfaro <ealfaro810@gmail.com>
2022-08-01 17:06:14 -0600

    Guardar y listar Pedidos

--- src/screens/pedidosLlevar/listarPedidosLlevar.js
+++ src/screens/pedidosLlevar/listarPedidosLlevar.js
@@ -17,7 +17,7 @@ import Axios from "../../components/Axios"
 
 const ListarPedidosLlevar = ({navigation}) => {
 	let textoMensaje = "";
-	const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZHJlZ2lzdHJvIjoxLCJpYXQiOjE2NTkxMDYyMjMsImV4cCI6MTY1OTEzNjIyM30.cVRlDuZWYCdo-rVn7Lje9cfqrjodTqaM72tF5kWCv18";
+	const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZHJlZ2lzdHJvIjoxLCJpYXQiOjE2NTkzODg0NzMsImV4cCI6MTY1OTQxODQ3M30.amS9NqKAO3mHfGdR6V8Eoy3OmbLkYMKc5kCj7C9tFV4";
 	const [lista, setLista] = useState([]);
 	const [filtro, setFiltro] = useState("");
 
